e29f7f67d9e7b151cc4f4fd06bb3a7f605544d16
bruno.Aquinol <br.aquino.oliveira@gmail.com>
2019-08-16 16:18:35 -0300

    imagem principal arrumada; imagem Produtos responsivo

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-						$nome = $_POST['nome'];
-						$email= $_POST['email'];
-						$mensagem= $_POST['mensagem'];
-						$to = "br.aquino.oliveira@gmail.com";
-						$assunto = "Mensagem de ".$email.com
-						mail($to,$assunto,$mensagem);
-					?>
-
